a5982361f460abccd9f994cbf7c40e46b50989d8
Hubertoos <Hubertoos@users.noreply.github.com>
2023-06-11 13:25:19 +0200

    Utworzenie pierwszej wersji
    Pierwszy commit do brancha main

---
+++ task/index.js
@@ -0,0 +1,42 @@
+import x from './example';
+
+
+function main() {
+    // actualPageIdx to index wybranej strony (indexujemy od 0)
+// entriesOnPage to maksymalna zwracana ilość elementów z dataEntries dla wybranej strony
+
+// przykładowe dane
+const data = [1,2,3,4,5,6];
+const settings = { actualPageIdx: 1, entriesOnPage: 3 }; 
+
+const paginateArray = (dataEntries, settings) => {
+  let entriesOnSelectedPage = [];
+  const startIdx = settings.actualPageIdx+1;
+  const entriesPerPage = settings.entriesOnPage;
+  const dataCount = dataEntries.length;
+  let endIdx = 0;
+  console.log(startIdx);
+  console.log(entriesPerPage);
+  console.log(dataCount);
+
+
+  if (startIdx+entriesPerPage>=dataCount) { 
+        endIdx=dataCount;
+    }
+  else {
+      endIdx=startIdx+entriesPerPage
+  }
+  console.log(endIdx);
+  for (let i = startIdx; i<endIdx; i++) {
+      entriesOnSelectedPage.push(dataEntries[i]);
+  }
+  
+  return entriesOnSelectedPage;
+};
+
+const result = paginateArray(data, settings);
+console.log(result);
+
+}
+
+export default main
